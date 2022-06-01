36c603476eb4e5d814b39d6f212237e838ecfe79
Hovhannes Kocharyan <hovkocharyan@yandex.com>
2022-06-01 15:07:59 +0400

    feat(post): create style for component

--- src/post/post.component.ts
+++ src/post/post.component.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
+  styleUrls: ['./post.component.scss'],
 })
 export class PostComponent {}
