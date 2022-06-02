e9636c2423e97fd7dfaf0e3074070c3134ba2346
Hovhannes Kocharyan <hovkocharyan@yandex.com>
2022-06-02 17:41:11 +0400

    feat(form): 2 way binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormComponent } from 'src/form/form.component';
 import { PostComponent } from 'src/post/post.component';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [AppComponent, PostComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, PostComponent, FormComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
