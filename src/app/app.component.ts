453487da6ba6db5a6cf59c37518ad22a96cf84e0
Hovhannes Kocharyan <hovkocharyan@yandex.com>
2022-06-08 18:06:31 +0400

    feat(post): add dynamic post

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { IPost } from 'src/types/post.types';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  posts: IPost[] = [
+    { id: 1, title: 'Title 1', text: 'Text 1' },
+    { id: 2, title: 'Title 2', text: 'Text 2' },
+    { id: 3, title: 'Title 3', text: 'Text 3' },
+  ];
+}
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { IPost } from 'src/types/post.types';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.scss']
+  styleUrls: ['./post.component.scss'],
 })
 export class PostComponent implements OnInit {
+  @Input() post: IPost;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
