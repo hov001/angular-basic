112cf48d02df73fd077f919532f3609569ee2bcc
Hovhannes Kocharyan <hovkocharyan@yandex.com>
2022-06-08 17:50:08 +0400

    feat(app): create post and post-form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { FormComponent } from 'src/form/form.component';
-import { PostComponent } from 'src/post/post.component';
+import { PostComponent } from './post/post.component';
 import { AppComponent } from './app.component';
+import { PostFormComponent } from './post-form/post-form.component';
 
 @NgModule({
-  declarations: [AppComponent, PostComponent, FormComponent],
+  declarations: [AppComponent, PostComponent, PostFormComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
