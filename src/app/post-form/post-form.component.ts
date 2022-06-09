b1651e515f40dd531dc88771340378283b1caa8c
Hovhannes Kocharyan <hovkocharyan@yandex.com>
2022-06-09 14:15:53 +0400

    feat(post-form): add ref in the node element

--- src/app/post-form/post-form.component.ts
+++ src/app/post-form/post-form.component.ts
@@ -1,6 +1,14 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { IPost } from 'src/types/post.types';
 import { v4 } from 'uuid';
+import { focusVariants } from 'src/types/focus.types';
 
 @Component({
   selector: 'app-post-form',
@@ -12,6 +20,8 @@ export class PostFormComponent implements OnInit {
   text = '';
 
   @Output() onAdd: EventEmitter<IPost> = new EventEmitter<IPost>();
+
+  @ViewChild('inputTitle') titleRef: ElementRef;
 
   constructor() {}
 
@@ -29,5 +39,9 @@ export class PostFormComponent implements OnInit {
 
       this.title = this.text = '';
     }
+  }
+
+  focusOnTitle() {
+    this.titleRef.nativeElement.focus();
   }
 }
---
+++ src/types/focus.types.ts
@@ -0,0 +1,4 @@
+export type focusVariants = {
+  TITLE: 'TITLE';
+  TEXT: 'TEXT';
+};
